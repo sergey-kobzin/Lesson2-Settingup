4cd15c606923129be4de14797f10681339c43df0
shpp-skobzin <sergey.kobzin@gmail.com>
2016-02-01 01:52:06 +0200

    All is OK

---
+++ app/src/main/java/com/shpp/skobzin/lesson2_settingup/MainActivity.java
@@ -0,0 +1,28 @@
+package com.shpp.skobzin.lesson2_settingup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setButtonClickListener();
+    }
+
+    private void setButtonClickListener() {
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view.setVisibility(View.GONE);
+            }
+        });
+    }
+}
